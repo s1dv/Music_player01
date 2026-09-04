4efee4690aa2515771643d25ce8e8aa4734cff05
s1dv <shawsiddhant025@gmail.com>
2026-09-05 02:53:55 +0530

    removed additional pygame dialogue

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import os
-
+#ignore pygame message annoying
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
 import pygame
 
+#main function
+def main():
+
+    try:
+        pygame.mixer.init()
+    except pygame.error as e:
+        print("audio initialization error", e)
+
+if __name__ == "__main__":
+    main()
