140bcc484337e9b27924ea0fa3158a44e6173a89
s1dv <shawsiddhant025@gmail.com>
2026-09-05 03:13:32 +0530

    added music folder and mp3 files support

--- main.py
+++ main.py
@@ -11,5 +11,20 @@ def main():
     except pygame.error as e:
         print("audio initialization error", e)
 
+    folder = "music"
+
+    if not os.path.isdir(folder):   #checking if the music folder is inside the root dir or not.
+        print(f"folder '{folder}' not found!")
+        return
+
+    #checking mp3 files
+
+    mp3_files = [file for file in os.listdir(folder) if file.endswith(".mp3")]
+    #if not mp3 files.
+    if not mp3_files:
+        print(" no mp3 files found")
+
+
+
 if __name__ == "__main__":
     main()
